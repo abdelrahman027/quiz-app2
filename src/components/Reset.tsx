4031f7c17fe1a012ace421fc299f393395ee2d9b
abdelrahman027 <abdelrahmansaad027@gmail.com>
2024-11-24 19:11:38 +0200

    make score sheet

--- src/components/Reset.tsx
+++ src/components/Reset.tsx
@@ -7,13 +7,25 @@ type Props = {
     correctQuestions: number;
     onPress: () => void;
 };
-
 function Reset(props: Props) {
+    const score = ((props.correctQuestions / props.totalQuestions) * 100).toFixed(2)
+    console.log(parseInt(score))
     return (
-        <div className='w-1/2 mx-auto mt-12'>
-            <h1 className={`${Reset_module['reset-text']} text-2xl`}>
-                You scored: {((props.correctQuestions / props.totalQuestions) * 100).toFixed(2)}%
+        <div className='w-1/2 mx-auto mt-12 bg-black/40 p-6 rounded-lg'>
+            <h1 className={`${Reset_module['reset-text']} text-2xl text-white`}>
+                You scored: {score}%
             </h1>
+            <p className='text-white text-xl'> You are {parseInt(score)>=50?"Left sided brain":"Right Sided Brain"}</p>
+            <button
+                            className={Classnames(
+                                App_module['next-btn'],
+                                Answer_module.answer,
+                                Reset_module['reset-btn']
+                                
+                            )}
+            >
+                Check Your Brain here
+            </button>
             <button
                 onClick={props.onPress}
                 className={Classnames(
